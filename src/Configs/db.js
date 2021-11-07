b447fb0c3606e5847ccc4ddab8589d9106a42040
Mohd Tausif <65054692+sheikhtausif@users.noreply.github.com>
2021-11-07 17:39:17 +0530

    Delete src directory

--- src/Configs/db.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_LOCAL_URI
-
-const connect = async () => {
-    return await mongoose.connect(MONGO_URI)
-}
-
-module.exports = connect
